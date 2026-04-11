7b558a8e779cc651fe73c9bfeb6f97f47df6209f
lika^-^ <likaianvarashvili3@gmail.com>
2026-04-11 12:52:37 +0400

    chore: setup tailwindcss v4 cli build process

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        switzer: ["Switzer", "sans-serif"],
+      },
+      colors: {
+        peach: "#fff1be",
+        pink: "#d77cde",
+        text: "#030712",
+        grey: "#555",
+      },
+    },
+  },
+  plugins: [],
+};
